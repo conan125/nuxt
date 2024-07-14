a206f200e61885d907fdaaf8d6e680451e50583f
matu-lgs <442285570@qq.com>
2024-07-14 12:41:57 +0900

    60

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
  * @Author: matu-lgs 442285570@qq.com
  * @Date: 2024-07-05 18:23:33
  * @LastEditors: matu-lgs 442285570@qq.com
- * @LastEditTime: 2024-07-14 11:41:19
+ * @LastEditTime: 2024-07-14 12:41:47
  * @FilePath: /bilibilbi/app.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -20,11 +20,6 @@
         >
       </li>
     </ul>
-
-    <ul>
-      <li v-for="item in [1, 2, 3]">{{ item }}</li>
-    </ul>
-    hellowo
   </div>
 </template>
 <script setup lang="ts">
